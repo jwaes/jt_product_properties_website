f4539eda277ddbfd8ce45aa7c34630f6d7249eb7
jwaes <jaco.waes@gmail.com>
2024-07-24 14:34:43 +0000

    er

--- __manifest__.py
+++ __manifest__.py
@@ -36,7 +36,7 @@
     'assets': {
         'web.assets_frontend': [
             'jt_product_properties_website/static/src/js/**/*',
-            'jt_product_properties_website/static/src/xml/**/*',
+            # 'jt_product_properties_website/static/src/xml/**/*',
         ],
     },    
 }
